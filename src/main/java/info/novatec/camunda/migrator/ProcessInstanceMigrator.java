7665e774ea94305962dbfa426fc02390ecef4523
BenFuernrohr <B_M_L@web.de>
2022-04-01 15:52:01 +0200

    41: extract MigrationInstructionCombiner

--- src/main/java/info/novatec/camunda/migrator/ProcessInstanceMigrator.java
+++ src/main/java/info/novatec/camunda/migrator/ProcessInstanceMigrator.java
@@ -1,13 +1,11 @@
 package info.novatec.camunda.migrator;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.impl.migration.MigrationInstructionImpl;
 import org.camunda.bpm.engine.migration.MigrationInstruction;
 import org.camunda.bpm.engine.migration.MigrationPlan;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
@@ -17,6 +15,7 @@ import info.novatec.camunda.migrator.instances.GetOlderProcessInstancesDefaultIm
 import info.novatec.camunda.migrator.instances.GetOlderProcessInstances;
 import info.novatec.camunda.migrator.instances.VersionedProcessInstance;
 import info.novatec.camunda.migrator.instructions.GetMigrationInstructions;
+import info.novatec.camunda.migrator.instructions.MigrationInstructionCombiner;
 import info.novatec.camunda.migrator.instructions.MigrationInstructions;
 import info.novatec.camunda.migrator.instructions.MinorMigrationInstructions;
 import info.novatec.camunda.migrator.plan.CreatePatchMigrationplan;
@@ -94,7 +93,7 @@ public class ProcessInstanceMigrator {
 									processInstance.getProcessVersion().getMinorVersion(),
 									newestProcessVersion.getMinorVersion(), newestProcessVersion.getMajorVersion());
 
-					List<MigrationInstruction> executableMigrationInstructions = combineMigrationInstructions(
+					List<MigrationInstruction> executableMigrationInstructions = MigrationInstructionCombiner.combineMigrationInstructions(
 							applicableMinorMigrationInstructions);
 
 					addInstructions(migrationPlan, executableMigrationInstructions);
@@ -188,45 +187,5 @@ public class ProcessInstanceMigrator {
         return Optional.ofNullable(latestProcessDefinition).map(processDefinition ->
                     new VersionedDefinitionId(ProcessVersion.fromString(processDefinition.getVersionTag()), processDefinition.getId()));
     }
-
-	private List<MigrationInstruction> combineMigrationInstructions(
-			List<MinorMigrationInstructions> applicableMinorMigrationInstructions) {
-		List<MigrationInstruction> instructionList = new ArrayList<>();
-		applicableMinorMigrationInstructions.stream()
-			.sorted(Comparator.comparingInt(MinorMigrationInstructions::getSourceMinorVersion))
-			// check every applicable minor-migration
-			.forEach(minorMigrationInstructions ->
-			    minorMigrationInstructions.getMigrationInstructions().stream()
-					// go through all instructions for every migration
-					.forEach(migrationInstruction -> {
-                        // check if a migration instruction exists, that has that migrationInstructions
-                        // source as a target, i.e. instruction 1 goes from activity1 to activity2 and
-                        // instruction 2 goes from activity2 to activity3
-						boolean migrationInstructionWasAlreadySet = false;
-						MigrationInstruction instructionToReplace = null;
-						for (MigrationInstruction alreadySetInstruction : instructionList) {
-							if (alreadySetInstruction.getTargetActivityId() == migrationInstruction
-									.getSourceActivityId()) {
-								migrationInstructionWasAlreadySet = true;
-								instructionToReplace = alreadySetInstruction;
-							}
-						}
-						// if such a migration instruction exists, remove it and replace it with a
-						// combined instruction
-						if (migrationInstructionWasAlreadySet && instructionToReplace != null) {
-							instructionList.remove(instructionToReplace);
-							instructionList.add(new MigrationInstructionImpl(
-									instructionToReplace.getSourceActivityId(),
-									migrationInstruction.getTargetActivityId(), true));
-						}
-						// if the instruction does not need to be combined, just add it to the list
-						else {
-							instructionList.add(new MigrationInstructionImpl(
-									migrationInstruction.getSourceActivityId(),
-									migrationInstruction.getTargetActivityId(), true));
-						}
-					}));
-		return instructionList;
-	}
 	
 }
